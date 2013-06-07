e8838af054ad536da96b2956469f423dd48a8e77
Oblosys <martijn@oblomov.com>
2013-06-07 20:41:10 +0000

    Replaced hand-written server with connect-style server
    git-svn-id: svn://svn.oblomov.com/Oblomov/JavaScript/Reservations@3772 63c81c92-1a08-11de-87e6-23293a004a03

--- Server.js
+++ Server.js
@@ -2,35 +2,29 @@
  * Web server for reservations app
  */
 
-var portNr = process.argv[2] || 8200;
+var portNr = process.argv[2] || 8200
+  , http = require('http')
+  , url = require('url')
+  , fs = require('fs')
+  , connect = require('connect')
+  , app;
 
-var http = require('http');
-var url = require('url');
-var path = require('path');
-var fs = require('fs');
-var qs = require('querystring');
-
-var mimeTypes = { "html": "text/html"
-				, "jpeg": "image/jpeg"
-				,"jpg": "image/jpeg"
-				, "png": "image/png"
-				, "js": "text/javascript"
-				, "css": "text/css"
-                };
 function writeError(res, nr, msg){
   res.writeHead(nr, {'Content-Type': 'text/plain'});
   res.write(msg);
   res.end();
 }
 
-
-function reqHandler(req, res) {
+function modelHandler(req, res) {
+  var uri = url.parse(req.url).pathname;
+  console.log('model Handler: '+req.method+' '+uri);
+  
   if (req.method == 'GET')
-    fileHandler(req,res);
+    res.end('{"x":10,"y":20,"z":30}');
   else if (req.method == 'POST')
     postHandler(req,res);
   else
-    writeError(res,405,'Unknown method: \''+req.method+'\'');
+    writeError(res,400,'Unhandled method: \''+req.method+'\'');
 }
 
 function postHandler(req, res) {
@@ -42,53 +36,20 @@ function postHandler(req, res) {
       console.log(postData);
   });
 }
-function fileHandler(req, res) {
-  var uri = url.parse(req.url).pathname;
-  console.log(req.method + ' ' +uri);
-  var filename = path.join( process.cwd()
-                          , path.join( 'www'
-                                     , unescape(uri == '/' ? 'index.html' : uri)));
 
-  try {
-    stats = fs.lstatSync(filename); // throws if path doesn't exist
-  } catch (e) {
-    writeError(res,404,'File not Found\n');
-    return;
-  }
-
-  if (stats.isFile()) {
-    // path exists, is a file
-    var mimeType = mimeTypes[path.extname(filename).split(".")[1]];
-    res.writeHead(200, {'Content-Type': mimeType} );
-
-    //try {
-      var fileStream = fs.createReadStream(filename);
-      fileStream.on('error', function() { 
-        writeError(res,404,'Cannot open file\n');
-      });
-      
-      //console.log('before');
-      fileStream.pipe(res);
-      //console.log('after'); // todo: even on exception in pipe, this code executes
-    //} catch (e) {
-    //  console.log('error reading file: '+e);
-    //};
-      
-  //} else if (stats.isDirectory()) {
-  // directory
-  } else {
-    // Symbolic link, other?
-    // TODO: follow symlinks?  security?
-    writeError(res, 404,'Incorrect type\n');
-  } 
-}
-
+// todo: still necessary?
 process.on('uncaughtException', function(err) {
   // handle the error safely
   console.log(err);
 });
 
 
-http.createServer(reqHandler).listen(portNr);
+app = connect()
+  //.use(connect.favicon())
+  .use(connect.logger('dev'))
+  .use(connect.static('www'))
+  .use('/model',modelHandler);
 
-console.log('Server running at http://127.0.0.1:'+portNr+'/');
+http.createServer(app).listen(portNr, function() {
+  console.log('Server running at http://127.0.0.1:'+portNr+'/');
+});
