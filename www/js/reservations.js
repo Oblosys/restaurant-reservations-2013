516d838ce77f33a948c12ea4a0709b7bb9a82714
Oblosys <martijn@oblomov.com>
2013-06-07 12:31:37 +0000

    Added jQuery + Created simple datastructure and some views
    git-svn-id: svn://svn.oblomov.com/Oblomov/JavaScript/Reservations@3767 63c81c92-1a08-11de-87e6-23293a004a03

--- www/js/reservations.js
+++ www/js/reservations.js
@@ -1,1 +1,20 @@
 console.log('executing reservations.js');
+$(document).ready(function(){
+  initialize();
+});
+
+var data = {x:1, y:2, z:3};
+
+function refresh() {
+  $('#field1X').val(data.x);
+  $('#field1Y').val(data.y);
+  $('#field1Z').val(data.z);
+  $('#field2X').val(data.x);
+  $('#field2Y').val(data.y);
+  $('#field2Z').val(data.z);
+}
+
+function initialize() {
+  refresh();
+}
+
