bafc0cdcf5d4613936d28b2887879aea26f724cc
Oblosys <martijn@oblomov.com>
2013-06-09 21:30:08 +0000

    Experiments with Backbone
    git-svn-id: svn://svn.oblomov.com/Oblomov/JavaScript/Reservations@3782 63c81c92-1a08-11de-87e6-23293a004a03

--- www/js/reservations.js
+++ www/js/reservations.js
@@ -3,34 +3,141 @@ $(document).ready(function(){
   initialize();
 });
 
-var Data = Backbone.Model.extend({
+var Reservation = Backbone.Model.extend({
   defaults: {
-    x: 1,
-    y: 2,
-    z: 3 
+    date: 'date',
+    name: 'name',
+    nrOfPeople: 2
   },
-  url: 'model'
+  urlRoot: '/model/reservation'
 });
 
-var data = new Data;
+var Day = Backbone.Model.extend({
+  defaults: {
+    name: 'name'
+//    reservation1 : new Reservation({name: 'Pino'}), 
+//    reservation2 : new Reservation({name: 'Tommie'}), 
+  },
+  urlRoot: '/model/day'
+});
+
+var Reservations = Backbone.Collection.extend({
+  model: Reservation,
+  url: '/model/reservations'
+});
+
+var day = new Day({name: 'Monday', reservations: new Reservations()});
+console.log('day is');
+
+var SomeReservations = Backbone.Model.extend({
+  url: '/model/somereservations'
+});
+
+
+//todo: use window.reservations?
+var reservations = new Reservations();
+//reservations.set('reservations', new Reservations);
+
+var reservation, reservation2;
 
 function initialize() {
-  data.fetch();
+  day.get('reservations').on("add", refreshReservations);
+  day.get('reservations').on("remove", refreshReservations);
+/*
+  var action = reservations.fetch({
+    success: function() {
+      console.log('reservations: ');
+      console.log(reservations.toJSON());
+      //refreshReservations();
+     },
+    error: function() {alert('err');}
+  });
+*/
+  reservation = new Reservation({ name:"Pino1"});
+  reservation2 = new Reservation({ name:"Pino2"});
+  saveAll([reservation, reservation2, day]);
+  console.log('after save');
+  //reservation.save({success: function(){reservation2.save();}});
+//reservation2.save();
+  //reservation2.save();
 
-  registerEditor(data, 'x', $('#field1X'));
-  registerEditor(data, 'y', $('#field1Y'));
-  registerEditor(data, 'z', $('#field1Z'));
-  registerEditor(data, 'x', $('#field2X'));
-  registerEditor(data, 'y', $('#field2Y'));
-  registerEditor(data, 'z', $('#field2Z'));
-//  console.log('data.get '+data.get('x'));
+  day.set('reservation1', reservation);
+  day.set('reservation2', reservation2);
+  //reservations.reservations.remove(reservation);
+  //day.save();
+  registerEditor(reservation, 'date', $('#field1X'));
+  registerEditor(reservation, 'name', $('#field1Y'));
+  registerEditor(reservation, 'nrOfPeople', $('#field1Z'));
+  registerEditor(reservation, 'date', $('#field2X'));
+  registerEditor(reservation, 'name', $('#field2Y'));
+  registerEditor(reservation, 'nrOfPeople', $('#field2Z'));
+  
+//  console.log('data.get '+reservation.get('x'));
 }
+function saveCont(o,c) {
+  o.save({ success: c() });
+}
+
+function saveAll(models) {
+   if (models.length == 0)
+     return;
+   else {
+     var model = models.shift();
+     console.log('Saving '+model.get('name')+ ' '+models.length);
+     model.save({},{success: function() {saveAll(models);} });
+   } 
+}
+
+function refreshReservations() {
+  console.log('refreshReservations()');
+  $('#reservationList').empty();
+  $('#reservationList').append( JSON.stringify(reservation.toJSON()) + '<br/>');
+  $('#reservationList').append( JSON.stringify(reservation2.toJSON()) + '<br/>');
+  $('#reservationList').append( JSON.stringify(day.toJSON()) + '<br/>');
+/*
+  $('#reservationList').append( '<table>' );
+  day.get('reservations').each(function(res) {
+    console.log('ja');
+    $('#reservationList').append( '<tr><td>' + res.get('date') + '</td><td>' + res.get('name') + '</td><td>' + res.get('nrOfPeople') + '</td><td>' + res.get('id') + '</td></tr>');
+  });
+  $('#reservationList').append( '</table>' );
+  */
+}
+
+
+function saveButton() {
+//  console.log(reservations.toJSON());
+  reservations.save();
+}
+
+function testButton() {
+  console.log('Test button pressed');
+  //reservations.create({ name:"Pino"});
+  reservation.set('name','Tommie');
+  //day.save();
+  day.destroy();
+
+  //reservations.add(reservation);
+ }
+
+function refreshButton() {
+  //console.log(reservations.toJSON());
+  refreshReservations();
+  console.log(day.get('name'));
+}
+
+
+
+
+
+//observing / editing values
+
 function registerEditor(model, prop, elt) {
   var $elt = $(elt);
   $elt.change( function () { model.set(prop,$elt.val()); } );
   initObserver(model,prop,$elt);
-
 }
+
 function initObserver(model, prop, elt) {
   elt.val(model.get(prop));
   model.on("change:"+prop,changeHandler,elt);
@@ -38,10 +145,6 @@ function initObserver(model, prop, elt) {
 
 function changeHandler(model,newVal) {
   this.val(newVal);
-  console.log(model.get('x'));
+//  console.log(model.get('x'));
 }
 
-function saveButton() {
-  console.log(data.toJSON());
-  data.save();
-}
