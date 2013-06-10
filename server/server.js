5752fa15df9b68d21764d770cf5aa5f52c44e207
Oblosys <martijn@oblomov.com>
2013-06-10 11:41:46 +0000

    Created all purpose CRUD server (non-persistent, for the moment)
    git-svn-id: svn://svn.oblomov.com/Oblomov/JavaScript/Reservations@3787 63c81c92-1a08-11de-87e6-23293a004a03

--- server/server.js
+++ server/server.js
@@ -8,10 +8,12 @@ var portNr = process.argv[2] || 8200
   , url = require('url')
   , fs = require('fs')
   , express = require('express')  
+  , _ = require('underscore')
   , Backbone = require('backbone')
   , app;
 
 function writeError(res, nr, msg){
+  console.error('ERROR '+nr+': '+msg);
   res.writeHead(nr, {'Content-Type': 'text/plain'});
   res.write(msg);
   res.end();
@@ -24,26 +26,83 @@ process.on('uncaughtException', function(err) {
 });
 
 
-var globalCounter = 0;
+/* Basic CRUD server that stores models in root.<modelname>.models and keeps track of id counter */
+var root = 
+  { //reservation: { idCounter: 10, models: [{id: "reservation-1", name: "Pino", date: "4-6-2013"},{id: "reservation-2", name: "Tommie", date: "5-6-2013"}]}
+  //, book:        { idCounter:100, models: [{id: "book-1", title: "Oblomov"},{id: "book-2", title: "War and peace"}]}            
+  };
+
+// todo: maybe create html errors? (need to pass res)
 
 function readModel(type, id) {
   console.log('\nREAD: type:'+type+' id:'+id);
-  return {id: id, name: "Pino "+type, date: "4-6-2013"};
+  
+  if (!root.hasOwnProperty(type))
+    console.error('Unknown type: '+type);
+  else {
+    var models = _.where(root[type].models, {id: id});
+    if (models.length==1)
+      return models[0];
+    else if (models.length<1)
+      console.error('Unknown id: '+id+' for type '+type);
+    else
+      console.error('Multiple ids: '+id+' for type '+type);
+  }
 }
-function createModel(type, obj) {
+
+// todo: what if object already has an id?
+function createModel(type, newModel) {
   console.log('\nCREATE: type:'+type);
-  console.log('content: '+JSON.stringify(obj));
-  id = type+'-'+globalCounter++;
+  console.log('content: '+JSON.stringify(newModel));
+  
+  if (!root.hasOwnProperty(type))
+    root[type] = {idCounter: 1, models: []};
+  
+  id = type+'-'+root[type].idCounter++;
   console.log('fresh id:'+id);
+  newModel.id = id;
+  root[type].models.push(newModel);
+  //console.log('root: '+JSON.stringify(root));
   return {id: id};
 }
-function updateModel(type, id, obj) {
+
+function updateModel(type, id, newModel) {
   console.log('\nUPDATE: type:'+type+' id:'+id);
-  console.log('content: '+JSON.stringify(obj));
+  console.log('content: '+JSON.stringify(newModel));
+
+  if (!root.hasOwnProperty(type))
+    console.error('Unknown type: '+type);
+  else {
+    var models = _.where(root[type].models, {id: id});
+    if (models.length==1) {
+      var ix = root[type].models.indexOf(models[0]);
+      root[type].models[ix] = newModel;
+    }
+    else if (models.length<1)
+      console.error('Unknown id: '+id+' for type '+type);
+    else
+      console.error('Multiple ids: '+id+' for type '+type);
+  }
+  //console.log('root: '+JSON.stringify(root));
   return {};
 }
 function deleteModel(type, id) {
   console.log('\nDELETE: type:'+type+' id:'+id);
+
+  if (!root.hasOwnProperty(type))
+    console.error('Unknown type: '+type);
+  else {
+    var models = _.where(root[type].models, {id: id});
+    if (models.length==1) {
+      var ix = root[type].models.indexOf(models[0]);
+      root[type].models.splice(ix,1);
+    }
+    else if (models.length<1)
+      console.error('Unknown id: '+id+' for type '+type);
+    else
+      console.error('Multiple ids: '+id+' for type '+type);
+  }
+  //console.log('root: '+JSON.stringify(root));
   return {};
 }
 
