035b3ee4e3141e4460d466a1481025b8d09855e8
Oblosys <martijn@oblomov.com>
2013-06-10 15:08:14 +0000

    Added backbone Reservation collection viewedMonth
    git-svn-id: svn://svn.oblomov.com/Oblomov/JavaScript/Reservations@3793 63c81c92-1a08-11de-87e6-23293a004a03

--- www/js/calendar.js
+++ www/js/calendar.js
@@ -3,6 +3,20 @@ $(document).ready(function(){
   initialize();
 });
 
+var Reservation = Backbone.Model.extend({
+  defaults: {
+    date: '1-1-2000',
+    name: 'name'
+  },
+  urlRoot: '/model/reservation'
+});
+
+var Reservations = Backbone.Collection.extend({
+  model: Reservation,
+  url: '/query/range?start=1-6-2013&end=30-6-2013'
+});
+var viewedMonth;
+
 var Selection = Backbone.Model.extend({});
 var selection = new Selection;
 
@@ -22,17 +36,44 @@ function initialize() {
     $(this).attr('id','day-'+i);
     $(this).html('<div class="dayContent">'+i+'</div>');
     $(this).click( function() { selection.set('day', this);} );
-    console.log('blaa:'+$(this));
   });
   $('.hourHeader .hour').each(function(i) {
     $(this).attr('id','hour-'+i);
     $(this).click( function() { selection.set('hour', this);} );
   });
+  
+
+  viewedMonth = new Reservations;
+  viewedMonth.on("add", function(mdl,cln,opts) {console.log('Reservation added '+mdl.get('name')); logViewedMonth();});
+  viewedMonth.on("remove", function() {console.log('Reservation added'); logViewedMonth();});
+  viewedMonth.fetch();
 }
 
 function clickDay(day) {
   selection.set('day', day);
 }
-function testButton() {
-  console.log('Test button pressed');
- }
+function logViewedMonth() {
+  $('#log').empty();
+  $('#log').append( JSON.stringify(viewedMonth) +'<br/>');
+}
+function testButton1() {
+  console.log('Test button 1 pressed');
+  console.log(JSON.stringify(viewedMonth));
+}
+function testButton2() {
+  console.log('Test button 2 pressed, create');
+  viewedMonth.create({name:'Ieniemienie', date: '10-6-2013'});
+}
+function testButton3() {
+  console.log('Test button 3 pressed, fetch');
+  viewedMonth.fetch();
+//  console.log(JSON.stringify(viewedMonth));
+}
+function testButton4() {
+  console.log('Test button 4 pressed');
+//  console.log(JSON.stringify(viewedMonth));
+}
+function refreshButton() {
+  console.log('Refresh button pressed');
+  logViewedMonth();
+}
