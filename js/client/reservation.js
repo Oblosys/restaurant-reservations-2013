4753efec1f56f8b649bbcacc397a7c60965eb235
Oblosys <martijn@oblomov.com>
2013-06-17 17:59:45 +0000

    Var for newReservation, which is correctly set by buttons
    git-svn-id: svn://svn.oblomov.com/Oblomov/JavaScript/Reservations@3839 63c81c92-1a08-11de-87e6-23293a004a03

--- js/client/reservation.js
+++ js/client/reservation.js
@@ -21,33 +21,62 @@ var Reservations = Backbone.Collection.extend({
 });
 var reservationsToday;
 
+var newReservation;
+
 function initialize() {
   console.log('initializing');
+  newReservation = new Reservation();
+  $('.NrOfPeopleButtons input').each(function(i) {
+    $(this).click(function() {
+        newReservation.set('nrOfPeople', i);
+    });
+  });
+  
+  var today = new Date();
+  var dayLabels = ['Zo','Ma','Di','Wo','Do','Vr','Za'];
+  var dateLabels = ['Today ('+util.showDate(today)+')','Tomorrow'];
+  var weekDay = today.getDay(); // 0 is Sunday
+  for (var d=0; d<6; d++)
+    dateLabels.push(dayLabels[(d+weekDay+2)%7]);
+  $('.LargeDayButtons input,.SmallDayButtons input').each(function(i) {
+    var buttonDate = new Date(today);
+    buttonDate.setDate( today.getDate() + i );
+    $(this).attr('value', dateLabels[i]);
+    $(this).click(function() {
+      newReservation.set('date', util.showDate(buttonDate));
+    });
+  });
+  
+  var timeLabels = [];
+  for (var hr=18; hr<25; hr++) {
+    timeLabels.push(hr+':00');
+    timeLabels.push(hr+':30');
+  }
+  $('.TimeButtons input').each(function(i) {
+    $(this).attr('value', timeLabels[i]);
+    $(this).click(function() {
+      newReservation.set('time', timeLabels[i]);
+    });
+  });
 }
 
 function log() {
   $('#log').empty();
-  $('#log').append( 'logging' +'<br/>');
+  $('#log').append( JSON.stringify(newReservation) +'<br/>');
 }
 function testButton1() {
   console.log('Test button 1 pressed');
   log();
-  //console.log(JSON.stringify(viewedMonth));
 }
 
 function testButton2() {
   console.log('Test button 2 pressed, create');
-  viewedMonth.create({name:'Ieniemienie', date: '1-6-2013', nrOfPeople: 2});
 }
 function testButton3() {
   console.log('Test button 3 pressed, remove');
-  viewedMonth.remove(viewedMonth.findWhere({name: 'Ieniemienie'}));
-  
-//  console.log(JSON.stringify(viewedMonth));
 }
 function testButton4() {
   console.log('Test button 4 pressed');
-//  console.log(JSON.stringify(viewedMonth));
 }
 function refreshButton() {
   console.log('Refresh button pressed');
