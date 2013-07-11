c2de54c9fa13399c9395657e33683c56f5d3818f
Oblosys <martijn@oblomov.com>
2013-07-11 15:38:45 +0000

    New versions for crud methods for experimenting with MySQL
    git-svn-id: svn://svn.oblomov.com/Oblomov/JavaScript/Reservations@3928 63c81c92-1a08-11de-87e6-23293a004a03

--- js/genericServer.js
+++ js/genericServer.js
@@ -115,17 +115,17 @@ function createApplication() {
   app.use(express.bodyParser());
 
   app.get('/model/:type/:id', function(req, res) {
-    res.send(readModel(req.params.type, req.params.id));
+    res.send(readModelSql(req.params.type, req.params.id));
   });
   app.post('/model/:type', function(req, res) { 
     console.log(req.body);
-    res.send(createModel(req.params.type, req.body));
+    res.send(createModelSql(req.params.type, req.body));
   });
   app.put('/model/:type/:id', function(req, res) {
-    res.send(updateModel(req.params.type, req.params.id, req.body));
+    res.send(updateModelSql(req.params.type, req.params.id, req.body));
   });
   app.del('/model/:type/:id', function(req, res) {
-    res.send(deleteModel(req.params.type, req.params.id));
+    res.send(deleteModelSql(req.params.type, req.params.id));
   });
   
   return app;
@@ -145,8 +145,82 @@ function createServer(app) {
   });
 }
 
-// todo: why module.exports?
+
+function readModelSql(type, id) {
+  console.log('\nREAD: type:'+type+' id:'+id);
+  
+  if (!root.hasOwnProperty(type))
+    console.error('Unknown type: '+type);
+  else {
+    var models = _.where(root[type].models, {id: id});
+    if (models.length==1)
+      return models[0];
+    else if (models.length<1)
+      console.error('Unknown id: '+id+' for type '+type);
+    else
+      console.error('Multiple ids: '+id+' for type '+type);
+  }
+}
+
+// todo: what if object already has an id?
+function createModelSql(type, newModel) {
+  console.log('\nCREATE: type:'+type);
+  console.log('content: '+JSON.stringify(newModel));
+  
+  if (!root.hasOwnProperty(type))
+    root[type] = {idCounter: 0, models: []};
+  
+  var id = type+'-'+root[type].idCounter++;
+  console.log('fresh id:'+id);
+  newModel.id = id;
+  root[type].models.push(newModel);
+  //console.log('root: '+JSON.stringify(root));
+  return {id: id};
+}
+
+function updateModelSql(type, id, newModel) {
+  console.log('\nUPDATE: type:'+type+' id:'+id);
+  console.log('content: '+JSON.stringify(newModel));
+
+  if (!root.hasOwnProperty(type))
+    console.error('Unknown type: '+type);
+  else {
+    var models = _.where(root[type].models, {id: id});
+    if (models.length==1) {
+      var ix = root[type].models.indexOf(models[0]);
+      root[type].models[ix] = newModel;
+    }
+    else if (models.length<1)
+      console.error('Unknown id: '+id+' for type '+type);
+    else
+      console.error('Multiple ids: '+id+' for type '+type);
+  }
+  //console.log('root: '+JSON.stringify(root));
+  return {};
+}
+function deleteModelSql(type, id) {
+  console.log('\nDELETE: type:'+type+' id:'+id);
+
+  if (!root.hasOwnProperty(type))
+    console.error('Unknown type: '+type);
+  else {
+    var models = _.where(root[type].models, {id: id});
+    if (models.length==1) {
+      var ix = root[type].models.indexOf(models[0]);
+      root[type].models.splice(ix,1);
+    }
+    else if (models.length<1)
+      console.error('Unknown id: '+id+' for type '+type);
+    else
+      console.error('Multiple ids: '+id+' for type '+type);
+  }
+  //console.log('root: '+JSON.stringify(root));
+  return {};
+}
+
+//todo: why module.exports?
 exports = module.exports = createApplication;
 exports.version = '0.1.0';
 exports.root = root;
 exports.createServer = createServer;
+exports.createModelSql = createModelSql;
