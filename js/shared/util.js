309a1f3107fc7c49bd2bddd0ad5c3ca4530c672a
Oblosys <martijn@oblomov.com>
2013-08-04 18:45:34 +0000

    Fixed scrollMinimal to work for arbitrary elements
    git-svn-id: svn://svn.oblomov.com/Oblomov/JavaScript/Reservations@3951 63c81c92-1a08-11de-87e6-23293a004a03

--- js/shared/util.js
+++ js/shared/util.js
@@ -90,28 +90,33 @@
 // Closure is to prevent declaring globals in browser.
 // in Browser, exports is bound to global util variable.
 
-
 if (typeof jQuery != 'undefined') {
   // Extend jQuery objects with a method to scroll a minimum amount to make the object visible.
-  // Copied from http://stackoverflow.com/questions/4217962/scroll-to-an-element-using-jquery
-  jQuery.fn.scrollMinimal = function(smooth) {
-    var cTop = this.offset().top;
-    var cHeight = this.outerHeight(true);
-    var windowTop = $(window).scrollTop();
-    var visibleHeight = $(window).height();
-  
-    if (cTop < windowTop) {
+  // Adapted from http://stackoverflow.com/questions/4217962/scroll-to-an-element-using-jquery
+  // to work for arbitrary elements rather than document.
+  // NOTE scrolling div needs: position: relative;
+  // TODO: figure out a way to do this without
+  jQuery.fn.scrollMinimal = function(target, smooth) {
+    //console.log(target);
+    var tTop = $(target).position().top;
+    var tHeight = target.outerHeight(true);
+    var sTop = this.scrollTop();
+    var sHeight = this.height();
+    //console.log('minscroll s:'+sTop+'x'+sHeight+' t:'+tTop+'x'+tHeight);
+    
+    if (tTop < 0) {
       if (smooth) {
-        $('body').animate({'scrollTop': cTop}, 'slow', 'swing');
+        $(this).animate({'scrollTop': sTop + tTop},'fast','linear');
       } else {
-        $(window).scrollTop(cTop);
+        $(this).scrollTop(sTop + tTop);
       }
-    } else if (cTop + cHeight > windowTop + visibleHeight) {
+    } else if (tTop + tHeight > sHeight) {
+      //util.log('need to scroll: '+(tTop + tHeight-sHeight));
       if (smooth) {
-        $('body').animate({'scrollTop': cTop - visibleHeight + cHeight}, 'slow', 'swing');
+        $(this).animate({'scrollTop': sTop + tTop + tHeight-sHeight},'fast','linear');
       } else {
-        $(window).scrollTop(cTop - visibleHeight + cHeight);
+        $(this).scrollTop(sTop +    tTop + tHeight-sHeight);
       }
     }
   };
-}
+ }
