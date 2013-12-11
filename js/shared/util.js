432d62a457614e258092d3e532c8068d00b8f0dd
Oblosys <martijn@oblomov.com>
2013-12-11 15:31:23 +0000

    Export using name of module instead of exports
    git-svn-id: svn://svn.oblomov.com/Oblomov/JavaScript/Reservations@4207 63c81c92-1a08-11de-87e6-23293a004a03

--- js/shared/util.js
+++ js/shared/util.js
@@ -1,32 +1,32 @@
-(function(exports){
+(function(util){
   if (typeof window == 'undefined') {
     var _ = require('underscore');
   } else {  // NOTE: take care that corresponding modules are included in the embedding html page
     var _ = window._;
-  }
+  };
 
-  var debug = true; // set to false on deployment
+  util.debug = true; // set to false on deployment
   
-  function log(msg) {
-    if (debug)
+  util.log = function(msg) {
+    if (util.debug)
       console.log(msg);
-  }
+  };
 
   // NOTE: replicated objects are only cloned on top-level
-  function replicate(n,x) {
+  util.replicate = function(n,x) {
     var xs = [];
     for (var i=0; i<n; i++) 
       xs.push(_.clone(x));
     return xs;
-  }
+  };
 
   // pad integer with leading 0's when necessary 
-  function padZero(l, n) {
+  util.padZero = function(l, n) {
     return ('0000000000000000000000'+n).slice(-l);
-  }
+  };
 
   // depth is to prevent hanging on circular objects
-  function showJSON(json,indent,depth) {
+  util.showJSON = function(json,indent,depth) {
     indent = indent || '';
     depth = depth || 0;
     var str = '';
@@ -39,7 +39,7 @@
           str += '[]';
         else {
           for (var i = 0; i<json.length; i++)
-            str += (i==0?'[ ':indent + ', ') + showJSON(json[i],'  '+indent, depth+1)+'\n';
+            str += (i==0?'[ ':indent + ', ') + util.showJSON(json[i],'  '+indent, depth+1)+'\n';
           str += indent + ']';
         }
       } 
@@ -52,7 +52,7 @@
             for (var i = 0; i<keys.length; i++)
               str += (i==0?'{ ':indent + ', ') + keys[i] + ':' +
               (typeof json[keys[i]] == 'object' ? '\n' + indent +'  ' : ' ') + // for object children start new line
-              showJSON(json[keys[i]],'  '+indent, depth+1)+'\n';
+              util.showJSON(json[keys[i]],'  '+indent, depth+1)+'\n';
             str += indent + '}';
           }
         }
@@ -61,44 +61,34 @@
       }
     }
     return str;
-  }
+  };
   
-  function showTime(date) {
-    return padZero(2, date.getHours()) + ':' + padZero(2, date.getMinutes()) + ':' + padZero(2, date.getSeconds());
-  }
+  util.showTime = function(date) {
+    return util.padZero(2, date.getHours()) + ':' + util.padZero(2, date.getMinutes()) + ':' + util.padZero(2, date.getSeconds());
+  };
 
-  function showDate(date) {
-    return padZero(2, date.getDate()) + '-' + padZero(2, date.getMonth()+1) + '-' + padZero(4, date.getFullYear());
-  }
+  util.showDate = function(date) {
+    return util.padZero(2, date.getDate()) + '-' + util.padZero(2, date.getMonth()+1) + '-' + util.padZero(4, date.getFullYear());
+  };
 
-  function readDate(dateStr) {
+  util.readDate = function(dateStr) {
     var parts = dateStr.split('-');
     if (parts.length == 3)
       return new Date(parts[2], parts[1]-1, parts[0]);
     else
       throw 'Exception: Incorrect date: "'+dateStr+'"';
-  }
+  };
 
   /* Set boolean DOM attribute for jQuery object $elt according to HTML standard.
    * (absence denotes false, attrName=AttrName denotes true) */
-  function setAttr($elt, attrName, isSet) {
+  util.setAttr = function($elt, attrName, isSet) {
     if (isSet) 
       $elt.attr(attrName, attrName);
     else
       $elt.removeAttr(attrName);  
-  }
+  };
 
-  exports.debug = debug;
-  exports.log = log;
-  exports.padZero = padZero;
-  exports.replicate = replicate;
-  exports.showJSON = showJSON;
-  exports.showTime = showTime;
-  exports.showDate = showDate;
-  exports.readDate = readDate;
-  exports.setAttr = setAttr;
-
-})(typeof exports == 'undefined'? this.util={}: exports);
+})(typeof exports == 'undefined' ? this.util={} : exports);
 // Module for loading by Node.js as well as browser.
 // Closure is to prevent declaring globals in browser.
 // in Browser, exports is bound to global util variable.
