c83629344de43c46f5f9878278bf3a0286cac117
Oblosys <martijn@oblomov.com>
2013-12-16 18:57:48 +0000

    Use npm oblo-util
    git-svn-id: svn://svn.oblomov.com/Oblomov/JavaScript/Reservations@4223 63c81c92-1a08-11de-87e6-23293a004a03

--- js/shared/config.js
+++ js/shared/config.js
@@ -2,6 +2,6 @@
  * Configuration module for easily having different configurations in test environment and deployment server.
  */
 
-util = require('./oblo-util', 'util');
+util = require('oblo-util', 'util');
 
 util.debug = true;
